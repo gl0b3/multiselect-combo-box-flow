bf17f4f924fd2c9247cf902a1027b1d348312293
Goran <gatanaso@users.noreply.github.com>
2020-04-24 16:28:36 +0200

    Using the 2.4.3 bugfixed version from the multiselect-combo-box JS component.

--- src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
+++ src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
@@ -87,7 +87,7 @@ import elemental.json.JsonValue;
  * @author gatanaso
  */
 @Tag("multiselect-combo-box")
-@NpmPackage(value = "multiselect-combo-box", version = "2.4.2")
+@NpmPackage(value = "multiselect-combo-box", version = "2.4.3")
 @JsModule("multiselect-combo-box/src/multiselect-combo-box.js")
 @JavaScript("frontend://multiselectComboBoxConnector.js")
 @JsModule("./multiselectComboBoxConnector-es6.js")
